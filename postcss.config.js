48f030a286056f9aa9051c3d0e6846afe2548763
Shaun Porwal <shaun.porwal@gmail.com>
2025-02-24 23:02:36 -0500

    Switch to Tailwind CSS v3 for better compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
-    tailwindcss: require.resolve('tailwindcss'),
-    autoprefixer: require.resolve('autoprefixer'),
-  },
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 }; 
